225b9d6f8f272dec537eb3d34c5b4f1e9deb3788
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2023-01-13 13:49:27 +0100

    Resolved header css bugs with main branch

--- templates/fil_principal/fil.php
+++ templates/fil_principal/fil.php
@@ -15,21 +15,23 @@
 <body>
     <header id="header">
         <div class="logo-header">
-            <a href="."><h1 class="titre">Opinion<span class="dot">.</span></h1></a>
+            <a href="."><h1 class="logo">Opinion<span class="dot">.</span></h1></a>
         </div>
-        <ul>
-            <li><a href=".">Accueil</a></li>
-            <li><a href=".">Fil d'actualité</a></li>
-            <li><a href="?page=user&user=<?= $userId ?>">Profil</a></li>
-            <li><a href="#">Déconnexion</a></li> <!-- TODO: supprimer le lien et ouvrir une popup avec du JS -->
-        </ul>
-        <div class="search-bar-header">
-            <input type="text" name="search" placeholder="Rechercher">
-        </div>
-        <div class="user-menu">
-            <!-- TODO: change default avatar to current user avatar, if exists -->
-            <span class="user-icon"><img id="user-img" src="public/img/no_icon.png" alt="icône utilisateur"></span>
-            <span id="user-triangle-menu"></span>
+        <div class="menu-header">
+            <ul>
+                <li><a href=".">Accueil</a></li>
+                <li><a href=".">Fil d'actualité</a></li>
+                <li><a href="?page=user&user=<?= $userId ?>">Profil</a></li>
+                <li><a href="#">Déconnexion</a></li> <!-- TODO: supprimer le lien et ouvrir une popup avec du JS -->
+            </ul>
+            <div class="search-bar-header">
+                <input type="text" name="search" placeholder="Rechercher">
+            </div>
+            <div class="user-menu">
+                <!-- TODO: change default avatar to current user avatar, if exists -->
+                <span class="user-icon"><img id="user-img" src="public/img/no_icon.png" alt="icône utilisateur"></span>
+                <span id="user-triangle-menu"></span>
+            </div>
         </div>
     </header>
 </body>
