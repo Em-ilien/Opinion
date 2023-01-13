108aec3824b4ac0df387f136817ebb409cb1d92c
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2023-01-13 09:40:15 +0100

    Making fil.php files

---
+++ templates/fil_principal/fil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="public/css/fil.css">
+  <link href="https://fonts.cdnfonts.com/css/coolvetica" rel="stylesheet">
+  <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet">
+  <link href="https://fonts.cdnfonts.com/css/comfortaa" rel="stylesheet">
+    <title>Opinion.</title>
+</head>
+<body>
+    <header id="header">
+        <div class="logo-header">
+            <a href="#"><h1 class="logo">Opinion.</h1></a>
+        </div>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Fil d'actualité</a></li>
+            <li><a href="#">Profil</a></li>
+            <li><a href="#">Déconnexion</a></li>
+        </ul>
+        <div class="search-bar-header">
+            <input type="text" name="search" placeholder="Rechercher">
+        </div>
+    </header>
+</body>
+</html>
