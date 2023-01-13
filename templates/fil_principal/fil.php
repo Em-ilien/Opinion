71c1fab5235541606b17055acb84017fe390571f
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2023-01-13 12:27:10 +0100

    Header almost finished

--- templates/fil_principal/fil.php
+++ templates/fil_principal/fil.php
@@ -3,24 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="public/css/fil.css">
-  <link href="https://fonts.cdnfonts.com/css/coolvetica" rel="stylesheet">
-  <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet">
-  <link href="https://fonts.cdnfonts.com/css/comfortaa" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/header.css">
+    <link href="https://fonts.cdnfonts.com/css/coolvetica" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/comfortaa" rel="stylesheet">
     <title>Opinion.</title>
 </head>
 <body>
     <header id="header">
         <div class="logo-header">
-            <a href="#"><h1 class="logo">Opinion.</h1></a>
+            <a href="./?page=fil_principal/fil"><h1 class="logo">Opinion.</h1></a>
         </div>
-        <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Fil d'actualité</a></li>
-            <li><a href="#">Profil</a></li>
-            <li><a href="#">Déconnexion</a></li>
-        </ul>
-        <div class="search-bar-header">
-            <input type="text" name="search" placeholder="Rechercher">
+        <div class="menu-header">
+            <ul>
+                <li><a href="./?page=fil_principal/fil">Accueil</a></li>
+            </ul>
+            <div class="search-bar-header">
+                <input type="text" name="query" placeholder="Rechercher">
+            </div>
+            <div class="user-menu">
+                <!-- TODO: change default avatar to current user avatar, if exists -->
+                <span class="user-icon"><img id="user-img" src="public/img/no_icon.png" alt="icône utilisateur"></span>
+                <span id="user-triangle-menu"></span>
+            </div>
         </div>
     </header>
 </body>
